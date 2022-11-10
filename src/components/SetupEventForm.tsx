12cee53f10b521e2d7a10959cdf8bfe6237e99d9
ical10 <mochhusnir@gmail.com>
2022-11-10 15:07:17 +0700

    feat: add new fields and create form validation

--- src/components/SetupEventForm.tsx
+++ src/components/SetupEventForm.tsx
@@ -2,13 +2,24 @@ import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import {ethers} from 'ethers';
 
 export default function SetupEventForm() {
   const [prizeAmount, setPrizeAmount] = useState('0');
   const [prizeTokenAddress, setPrizeTokenAddress] = useState('');
+
   const [firstWinnerPercentage, setFirstWinnerPercentage] = useState('50');
   const [secondWinnerPercentage, setSecondWinnerPercentage] = useState('30');
   const [thirdWinnerPercentage, setThirdWinnerPercentage] = useState('20');
+
+  const [stringVoters, setStringVoters] = useState('');
+  const [votersConfirmed, setVotersConfirmed] = useState();
+  const [voters, setVoters] = useState([]);
+
+  const [stringParticipants, setStringParticipants] = useState('');
+  const [participantsConfirmed, setParticipantsConfirmed] = useState();
+  const [participants, setParticipants] = useState([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPrizeAmount(event.target.value);
@@ -30,6 +41,68 @@ export default function SetupEventForm() {
     setThirdWinnerPercentage(event.target.value);
   };
 
+  const handleChangeVoters = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStringVoters(event.target.value);
+  };
+
+  const convertVotersIntoArray = () => {
+    const temp = stringVoters.replace(/[\r\n]/gm, '').split(',');
+    setVoters(temp);
+    setVotersConfirmed(true);
+  };
+
+  const clearVoters = () => {
+    setStringVoters('');
+    setVoters([]);
+    setVotersConfirmed(false);
+  };
+
+  const handleChangeParticipants = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStringParticipants(event.target.value);
+  };
+
+  const convertParticipantsIntoArray = () => {
+    const temp = stringParticipants.replace(/[\r\n]/gm, '').split(',');
+    setParticipants(temp);
+    setParticipantsConfirmed(true);
+  };
+
+  const clearParticipants = () => {
+    setStringVoters('');
+    setVoters([]);
+    setVotersConfirmed(false);
+  };
+
+  const isFormValid = () => {
+    const {utils} = ethers;
+
+    if (
+      Number(prizeAmount) > 0 ||
+      utils.isHexString(prizeTokenAddress) ||
+      Number(firstWinnerPercentage) ||
+      Number(secondWinnerPercentage) ||
+      Number(thirdWinnerPercentage) ||
+      voters.length > 0 ||
+      participants.length > 0
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleSubmit = () => {
+    console.log({
+      prizeAmount,
+      prizeTokenAddress,
+      firstWinnerPercentage,
+      secondWinnerPercentage,
+      thirdWinnerPercentage,
+      voters,
+      participants,
+    });
+  };
+
   return (
     <Box
       component="form"
@@ -37,13 +110,18 @@ export default function SetupEventForm() {
         width: 'fit-content',
         borderRadius: '10px',
         padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '0.5rem',
         background: 'white',
         boxShadow: '0px 6px 10px rgba(0, 0, 0, 0.05)',
         '& .MuiTextField-root': {m: 1},
       }}
       noValidate
       autoComplete="off">
-      <Typography>Setup Event Form</Typography>
+      <Typography variant="h3" color="primary">
+        Setup your own event
+      </Typography>
       <div>
         <TextField
           id="prize-amount"
@@ -85,6 +163,54 @@ export default function SetupEventForm() {
           value={thirdWinnerPercentage}
           onChange={handleChangeThirdWinner}
         />
+      </div>
+
+      {/**Voters section**/}
+      <div>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <TextField
+            id="voters"
+            label="List of Voters"
+            variant="filled"
+            multiline
+            disabled={votersConfirmed}
+            value={stringVoters}
+            onChange={handleChangeVoters}
+          />
+        </div>
+        <Button
+          onClick={convertVotersIntoArray}
+          disabled={votersConfirmed || stringVoters.length === 0}>
+          Confirm Voters
+        </Button>
+        <Button onClick={clearVoters}>Clear Voters</Button>
+      </div>
+
+      {/**Participants section**/}
+      <div>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <TextField
+            id="participants"
+            label="List of Participants"
+            variant="filled"
+            multiline
+            disabled={participantsConfirmed}
+            value={stringParticipants}
+            onChange={handleChangeParticipants}
+          />
+        </div>
+        <Button
+          onClick={convertParticipantsIntoArray}
+          disabled={participantsConfirmed || stringParticipants.length === 0}>
+          Confirm Participants
+        </Button>
+        <Button onClick={clearParticipants}>Clear Participants</Button>
+      </div>
+
+      <div style={{marginTop: '1rem'}}>
+        <Button variant="outlined" onClick={handleSubmit} disabled={!isFormValid()}>
+          Create Event
+        </Button>
       </div>
     </Box>
   );
